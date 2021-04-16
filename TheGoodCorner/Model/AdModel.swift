95ba574f0252677802b027dfd7780e39a04ba4de
sugeethkumar <sugeethpro@hotmail.com>
2021-04-16 19:01:12 +0200

    Created ad model files.

---
+++ TheGoodCorner/Model/AdModel.swift
@@ -0,0 +1,39 @@
+//
+//  AdModel.swift
+//  TheGoodCorner
+//
+//  Created by Vigneswaranathan Sugeethkumar on 16/04/2021.
+//
+
+import Foundation
+
+// MARK: - WelcomeElement
+struct Ad: Codable {
+    let id, categoryID: Int
+    let title, welcomeDescription: String
+    let price: Int
+    let imagesURL: ImagesURL
+    let creationDate: Date
+    let isUrgent: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case categoryID = "category_id"
+        case title
+        case welcomeDescription = "description"
+        case price
+        case imagesURL = "images_url"
+        case creationDate = "creation_date"
+        case isUrgent = "is_urgent"
+    }
+}
+
+// MARK: - ImagesURL
+struct ImagesURL: Codable {
+    let small, thumb: String
+}
+
+struct Category: Codable {
+    let id: Int
+    let name: String
+}
