2b6177dce9c9363f96fb668c586031e41804c038
sugeethkumar <sugeethpro@hotmail.com>
2021-04-16 18:11:07 +0200

    Initial Commit

---
+++ TheGoodCorner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheGoodCorner
+//
+//  Created by Vigneswaranathan Sugeethkumar on 16/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
